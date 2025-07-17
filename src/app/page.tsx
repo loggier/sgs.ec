faba26d64f1d95e28595ef978b81e389fac7f932
Tracker Parser <info@trackerparser.com>
2025-07-17 02:22:47 +0000

    asi esta mi regla de acceso rules_version = '2';
    service cloud.firestor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,9 @@ export default async function Home() {
     <div className="flex flex-col h-screen">
       <Header title="Clientes" />
       <main className="flex-1 overflow-y-auto p-4 md:p-6">
-        <ClientList initialClients={clients} />
+        <div className="flex flex-col gap-6">
+          <ClientList initialClients={clients} />
+        </div>
       </main>
     </div>
   );
