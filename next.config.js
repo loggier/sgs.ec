c70f7a65589c7cb2fde5737e1ac58154df8810cc
Tracker Parser <info@trackerparser.com>
2026-01-02 18:35:49 +0000

    The app isn't starting. Please investigate what could be wrong based on

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+    ],
+  },
+  serverActions: {
+    bodySizeLimit: '10mb',
+  },
+}
+
+module.exports = nextConfig
